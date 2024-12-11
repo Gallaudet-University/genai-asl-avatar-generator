7e9598e765f1afb08bc760f87ee04af03df994e1
Jonah Winninghoff <52142917+jonahwinninghoff@users.noreply.github.com>
2024-12-11 10:20:58 -0800

    Update create_sign_videos.py

--- dataprep/create_sign_videos.py
+++ dataprep/create_sign_videos.py
@@ -141,9 +141,7 @@ def process_video(video, checkpoint_video_id,checkpoint_filepath):
         checkpoint_video_id = None
         return
     else:
-        raise Exception(
-            "Checkpoint video_id {} not valid".format(checkpoint_video_id)
-        )
+        return
     print("Downloading {} with video_id {}".format(video.url, video.video_id))
     download_dir = os.path.join(DATA_DIR, VIDEO_DOWNLOAD_DIR)
     video_filepath = download_large_file(
@@ -320,7 +318,6 @@ def create_sign_videos():
         partition_id, number_partitions
     )
     for video in videos:
-        print(video.video_id)
         process_video(video, checkpoint_video_id,checkpoint_filepath)
 
 
